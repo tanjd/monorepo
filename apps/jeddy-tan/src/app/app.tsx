a0db0b13175b1466ba306e1df5f3f72270bac846
Jeddy Tan <tanjd@hotmail.com>
2023-04-03 00:19:42 +0800

    Created first react application

---
+++ apps/jeddy-tan/src/app/app.tsx
@@ -0,0 +1,13 @@
+import NxWelcome from "./nx-welcome";
+
+export function App() {
+  return (
+    <>
+      <NxWelcome title="jeddy-tan" />
+
+      <div />
+    </>
+  );
+}
+
+export default App;
